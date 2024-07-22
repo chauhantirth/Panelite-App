049a262bcc0ca5599c77155b8b3be9cf4790c943
chauhantirth <chauhantirth40@gmail.com>
2024-07-22 20:34:34 +0530

    Added Login Screen + api, Solved workflow + AsyncStorage

--- app/index.js
+++ app/index.js
@@ -1,25 +1,94 @@
 import { useEffect, useState } from "react";
-import { View, SafeAreaView, Text } from "react-native";
+import { ActivityIndicator, View, SafeAreaView, Text } from "react-native";
 import { useRootNavigationState, router, useNavigation } from "expo-router";
+import * as SecureStore from 'expo-secure-store';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { validity } from "../components/api";
 
 const Main = () => {
+    console.log("=====================================")
     const [initLoad, setInitLoad] = useState(true);
-    // const token = null;
+    const [nextPage, setNextPage] = useState('login/');
 
+    const [isLoading, setIsLoading] = useState(false);
+
+    const [accessToken, setAccessToken] = useState(null);
     // const navigation = useNavigation();
-    // const rootNavigationState = useRootNavigationState();
+    //const rootNavigationState = useRootNavigationState();
+
+    const save = async(key, value) => {
+        setAccessToken(value);
+        AsyncStorage.setItem(key, value);
+    }
+
+    const deleteToken = async(key) => {
+        setAccessToken(null);
+        AsyncStorage.removeItem(key)
+    }
+
+    const retrieve = async (key) => {
+        const result = await AsyncStorage.getItem(key)
+        // console.log(result)
+
+        if (result) {
+            console.log('Found Token: '+result);
+
+            // success: https://rentry.co/horg2r5p/raw
+            // failure: https://rentry.co/krwmtcf5/raw
+
+            //check token validity
+            setIsLoading(true);
+            validity(
+                'https://rentry.co/horg2r5p/raw',
+                accessToken
+            ).then((data) => {
+                setIsLoading(false);
+                // console.log(data)
+                if (data.status == 'failure') {
+                    console.log("Token Invalid, move to login.");
+                    setNextPage('login/');
+                    router.push('login')
+                } else {
+                    console.log("Valid Token, Go to Dashboard.");
+                    setNextPage('/dashboard');
+                    router.push('login')
+                }
+            })
+
+        } else {
+            console.log("Not Found, move to login");
+            // save('accessToken', 'thisIsTheAccessToken');
+            setNextPage('login/');
+            router.push('login')
+        }
+    }
+
 
     useEffect(() => {
+
         const timer = setTimeout(() => {
             setInitLoad(false);
+            retrieve('accessToken');
         }, 3000);
+
+        //Checking for cached Auth Token...
+        // deleteToken('accessToken')
 
         // Clear the timeout if the component unmounts
         return () => clearTimeout(timer);
     }, []);
 
     if (initLoad) {
-        return <Text>Panelite.</Text>;
+        return (<>
+            <Text>
+                Panelite.
+            </Text>
+            {!isLoading ? (<></>) : (
+                <ActivityIndicator />
+            )} 
+            </>
+        );
+
     }
 
     return (
