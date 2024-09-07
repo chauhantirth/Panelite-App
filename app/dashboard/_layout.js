cdb03ccf6237545e010bef1c2bc8b384d55637fc
chauhantirth <chauhantirth40@gmail.com>
2024-09-07 18:12:42 +0530

    Update: Custom TabBar instead of stock

--- app/dashboard/_layout.js
+++ app/dashboard/_layout.js
@@ -1,17 +1,22 @@
 import { Tabs } from 'expo-router';
+import TabBar from '../../components/TabBar';
 
 const _layout = () => {
     return (
-        <Tabs>
+        <Tabs 
+            tabBar={props => <TabBar  {...props} />}
+        >
             <Tabs.Screen
                 name='index'
                 options={{
+                    title: "Home",
                     headerShown: false,
                 }}
             />
             <Tabs.Screen
                 name='profile'
                 options={{
+                    title: "Profile",
                     headerShown: false,
                 }}
             />
