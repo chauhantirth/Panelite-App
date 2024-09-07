cd4c1ba694bb045a6b352b4a0eae1a789a9a3c7e
chauhantirth <chauhantirth40@gmail.com>
2024-09-07 13:53:52 +0530

    Remove: Top Header

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,7 @@
 import { Stack } from "expo-router";
 
 const Layout = () => {
-    return <Stack screenOptions={{headerShown: true}}/>
+    return <Stack screenOptions={{headerShown: false}}/>
 }
 
 export default Layout;
