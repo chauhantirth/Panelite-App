8195ad1bb78ec51238f4d0f8c437efec156b5476
chauhantirth <chauhantirth40@gmail.com>
2024-09-08 21:44:12 +0530

    Add: Profile Page + formatting, Fetch for logout, networkError Handling, loader image for button

--- app/dashboard/profile.js
+++ app/dashboard/profile.js
@@ -1,106 +1,288 @@
-import { View, SafeAreaView, Text, Alert, TouchableOpacity } from 'react-native';
-import ColorList from '../../components/ColorList';
+import { View, SafeAreaView, Text, Alert, TouchableOpacity, StyleSheet, ScrollView, Image } from 'react-native';
+// import ColorList from '../../components/ColorList';
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { router } from 'expo-router';
 import { logout } from "../../components/api";
 import { useState, useEffect } from 'react';
+import loaderImage from '../../assets/fade-stagger-circles.png'
 
 const Profile = () => {
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState(null);
-    const [userSession, setUserSession] = useState(null);
+
+    const [userSessionData, setUserSessionData] = useState({});
+    const [sessionToken, setSessionToken] = useState({});
+
+    const [logoutLoading, setLogoutLoading] = useState(false);
+    const [networkError, setNetworkError] = useState(null);
+    
     // success: https://rentry.co/9bqubcgh/raw
+    // failure: https://rentry.co/3pwr53xd/raw
     
     const handleLogout = async(key) => {
-        setIsLoading(true);
+        setLogoutLoading(true);
+        setNetworkError(null);
+
         try {
-            logout(
-                'https://rentry.co/9bqubcgh/raw',
-                userSession
-            ).then((data) => {
-                if (data.status == "success") {
-                    setIsLoading(false)
+            const response = await fetch(
+                "https://rentry.co/3pwr53xd/raw", {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }, 
+                    // body: JSON.stringify({
+                    //     label: itemName,
+                    //     value: itemName,
+                    //     price: itemPrice
+                    // })
+            });
+            setLogoutLoading(false);
+    
+            if (!response.ok) {
+                setNetworkError('Something went wrong, please try again later.')
+                // throw new Error('Failed to push data to Database.');
+                return;
+            }
+    
+            const resData = await response.json();
+            if (resData) {
+                if (resData.status === "success") {
                     AsyncStorage.removeItem(key)
                     console.log("Successfully Logged Out.")
                     router.replace('login')
                 } else {
-                    setIsLoading(false);
                     console.log("Logout Failure.")
-                    setError(data.errorMsg)
+                    setNetworkError(resData.errorMsg)
                 }
-            }).catch( err => {
-                console.error(err)
-                setIsLoading(false)
-                setError("Something went wrong.")
-            });
+            } else {
+                setNetworkError('An Error occured parsing the server response, Try again later.')
+            }
         } catch (e) {
-            setIsLoading(false)
-            setError("Something went wrong, check your internet connection and try again.")
+            setLogoutLoading(false)
+            setNetworkError("Something went wrong, check your internet connection and try again.")
             console.log(e)
         }
     }
 
 
     const retrieve = async(key) => {
-        const userSession = await AsyncStorage.getItem(key);
-        setUserSession(userSession);
+        const result = await AsyncStorage.getItem(key);
+        if(result) {
+            const rs = JSON.parse(result)
+            setUserSessionData(JSON.parse(atob(rs.userData)));
+            setSessionToken(rs.sessionToken)
+        }
     }
 
     useEffect(() => {
         retrieve('userSession');
     }, [])
 
-    // useEffect(() => {
-    //     console.log("Triggered.")
-    //     handleLogout(accessToken);
-    // }, [triggerLogout])
-
-    // const deleteToken = async(key) => {
-    //     const accessToken = await AsyncStorage.getItem(key)
-    //     setIsLoading(true);
-
-    //     logout(
-    //         'https://rentry.co/ix87d4if/raw', accessToken
-    //     ).then((data) => {
-    //         setIsLoading(false);   
-    //         if (data.success) {
-    //             AsyncStorage.removeItem(key)
-    //             setIsLoading(false);
-    //             console.log("Successfully Logged Out");
-    //             router.replace('login');
-    //         } else {
-    //             setIsLoading(false);
-    //             console.log("Something went wrong.");
-    //             setError(data.errorMsg);
-    //         }
-    //     }).catch(err => {
-    //         setIsLoading(false);
-    //         console.error(err);
-    //         setError('Something went wrong.')
-    //     });
-    // }
-
-
     return (
         <SafeAreaView>
-            <TouchableOpacity onPress={() => {
-                setError(null);
-                Alert.alert("Logout", "Are you sure, you want to Logout ?", [
-                    {
-                        text: 'Cancel',
-                        onPress: () => {},
-                    },
-                    {
-                        text: 'Logout',
-                        onPress: () => {handleLogout('userSession')},
-                    }
-                ]);
-            }}>
-            <Text>Log Out</Text>
-            </TouchableOpacity>
-            <ColorList color="#0891b2"/>
+            <ScrollView contentContainerStyle={styles.container}>
+                <View style={styles.subcontainer}>
+                    <View style={styles.scTwoMain}>
+                        <View style={styles.scTwoLeft}>
+                            <Text style={styles.scTwoHeading}>
+                                Profile Settings
+                            </Text>
+                        </View>
+                    </View>
+                    <View
+                        style={styles.scSeparatingLine}
+                    />
+
+                    <View style={styles.scTwoBottom} >
+                        <Text style={styles.scTwoHeading2}>Email</Text>
+                        {userSessionData ? (
+                            <Text style={[styles.scTwoHeadingVal2]}>
+                                {userSessionData.userEmail}
+                            </Text>
+                        ) : (<></>)}
+                    </View>
+                    <View
+                        style={styles.scSeparatingLine}
+                    />
+
+                    <View style={styles.scTwoBottom} >
+                        <Text style={styles.scTwoHeading2}>Contact Number</Text>
+                        {userSessionData ? (
+                            <Text style={[styles.scTwoHeadingVal2]}>
+                                {userSessionData.userMobile}
+                            </Text>
+                        ) : (<></>)}
+                    </View>
+                    <View
+                        style={styles.scSeparatingLine}
+                    />
+
+                    <View style={styles.scTwoBottom} >
+                        <Text style={styles.scTwoHeading2}>App Version</Text>
+                        {userSessionData ? (
+                            <Text style={[styles.scTwoHeadingVal2]}>
+                                1.08.68
+                            </Text>
+                        ) : (<></>)}
+                    </View>
+
+                    {logoutLoading ? (
+                        <View style={styles.scThreeButtonContainerDisable}>
+                            <TouchableOpacity 
+                            disabled={logoutLoading}
+                            style={styles.scThreeButton}
+                        >
+                            <Image source={loaderImage} style={styles.scThreeCalenderImg} />
+                        </TouchableOpacity>
+                    </View>
+                    ) : (
+                        <View style={styles.scThreeButtonContainer}>
+                            <TouchableOpacity onPress={() => {
+                                setNetworkError(null);
+                                Alert.alert("Logout", "Are you sure, you want to Logout ?", [
+                                    {
+                                        text: 'Cancel',
+                                        onPress: () => {},
+                                    },
+                                    {
+                                        text: 'Logout',
+                                        onPress: () => {handleLogout('userSession')},
+                                    }
+                                ]);
+                                }}
+                                style={styles.scThreeButton}
+                            >
+                                <Text style={styles.scThreeButtonText}>Log Out</Text>
+                            </TouchableOpacity>
+                        </View>
+                    )}
+                </View>
+
+                {networkError ? (
+                    <View style={styles.networkErrorBox}>
+                        <Text style={styles.networkErrorText}>{networkError}
+                        </Text>
+                    </View>
+                ) : (<></>)}
+            </ScrollView>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    networkErrorText: {
+        textAlign: 'center',
+        fontSize: 14,
+        fontWeight: '300',
+        color: 'red',
+    },  
+    networkErrorBox: {
+        width: 'auto',
+        marginHorizontal: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: 'auto'
+    },  
+    scThreeButtonText: {
+        fontSize: 18,
+        fontWeight: '600',
+        marginVertical: 8,
+        color: 'black'
+    },
+    scThreeButton: {
+        width: '100%',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    scThreeButtonContainer: {
+        marginHorizontal: 20,
+        marginVertical: 2,
+        width: 'auto',
+        height: 40,
+        borderRadius: 45,
+        borderCurve: 'continuous', 
+        marginBottom: 15,
+        backgroundColor: '#0891b2', 
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowRadius: 10,
+        shadowOpacity: 0.2      
+    },
+    scThreeCalenderImg: {
+        marginTop: 8,
+        width: 24,
+        height: 24,
+        resizeMode: "cover",
+        marginBottom: 8,
+    },
+    scThreeButtonContainerDisable: {
+        marginHorizontal: 20,
+        marginVertical: 2,
+        width: 'auto',
+        height: 40,
+        borderRadius: 45,
+        borderCurve: 'continuous', 
+        marginBottom: 15,
+        backgroundColor: '#737373', 
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowRadius: 10,
+        shadowOpacity: 0.2      
+    },
+    scSeparatingLine: {
+        borderBottomColor: 'black',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+        marginHorizontal: 25
+    },
+    scTwoHeading2: {
+        fontSize: 14,
+        fontWeight: '300'
+    },
+    scTwoHeadingVal2: {
+        fontSize: 16,
+        fontWeight: '500'
+    },
+    scTwoBottom: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginVertical: 14,
+        marginHorizontal: 25
+    },
+    scTwoHeading: {
+        fontSize: 28,
+        fontWeight: '700',
+    },
+    scTwoLeft: {
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 14,
+        marginLeft: 25,
+    },
+    scTwoMain: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        gap: 80
+    },
+    subcontainer: {
+        flexDirection: 'column',
+        width: '100%',
+        height: 'auto',
+        borderRadius: 25,
+        borderCurve: 'continuous', 
+        marginBottom: 15,
+        backgroundColor: 'white', 
+        shadowColor: 'black',
+        shadowOffset: {width: 0, height: 0},
+        shadowRadius: 10,
+        shadowOpacity: 0.05
+    },
+    container: {
+      paddingHorizontal: 20, 
+      paddingVertical: 10, 
+      height: '100%'
+    }
+})
 
 export default Profile;
